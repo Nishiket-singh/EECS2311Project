36b9eabb4b32018abd5beaa8aca891da832af4ea
Nishiket-singh <nishiketsingh.0@gmail.com>
2023-02-13 18:19:07 -0500

    response for api calls

---
+++ lib/src/main/java/model/SourcesResponse.java
@@ -0,0 +1,24 @@
+package model;
+
+import java.util.List;
+
+public class SourcesResponse {
+    private String status;
+    private List<Source> sources;
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public List<Source> getSources() {
+        return sources;
+    }
+
+    public void setSources(List<Source> sources) {
+        this.sources = sources;
+    }
+}
