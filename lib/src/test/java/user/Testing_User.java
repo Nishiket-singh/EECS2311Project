f044cdf9d2bfb2982178288d560e4bf7e6c23403
eshavij <esha.vij@hotmail.com>
2023-02-15 20:26:55 -0500

    Finished Ratings Testcases

--- lib/src/test/java/user/Testing_User.java
+++ lib/src/test/java/user/Testing_User.java
@@ -69,6 +69,57 @@ class Testing_User {
 		c.getComments().add("This is another test comment.");
 		assertEquals(2, c.getComments().size());
 	}
+	
+	public class RatingsTest {
+		
+		@Test
+		public void testGetDislikeEmpty() {
+			Ratings r = new Ratings("Shirley Frod", 30, "Female");
+			assertTrue(r.getDislike().isEmpty());
+		}
+		
+		@Test
+		public void testSetDislike() {
+			Ratings r = new Ratings("Ken Shelly", 25, "Male");
+			ArrayList<String> dislike = new ArrayList<String>();
+			dislike.add("item1");
+			dislike.add("item2");
+			r.setDislike(dislike);
+			assertEquals(dislike, r.getDislike());
+		}
+		
+		@Test
+		public void testGetLikeEmpty() {
+			Ratings r = new Ratings("Daniel Hastings", 45, "Non-binary");
+			assertTrue(r.getLike().isEmpty());
+		}
+		
+		@Test
+		public void testSetLike() {
+			Ratings r = new Ratings("Donald Robertson", 20, "Male");
+			ArrayList<String> like = new ArrayList<String>();
+			like.add("item3");
+			like.add("item4");
+			r.setLike(like);
+			assertEquals(like, r.getLike());
+		}
+		
+		@Test
+		public void testMultipleLikes() {
+			Ratings r = new Ratings("Sarah Jessica Parker", 35, "Female");
+			r.getLike().add("item5");
+			r.getLike().add("item6");
+			assertEquals(2, r.getLike().size());
+		}
+		
+		@Test
+		public void testMultipleDislikes() {
+			Ratings r = new Ratings("Peter Gho", 45, "Non-binary");
+			r.getDislike().add("item7");
+			r.getDislike().add("item8");
+			assertEquals(2, r.getDislike().size());
+		}
+	}
 }
 
 
