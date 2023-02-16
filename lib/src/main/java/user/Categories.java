77fb1c81ede2b651067719bc6f5ff8619690814a
eshavij <esha.vij@hotmail.com>
2023-02-15 19:14:33 -0500

    Updated Categories.java class

--- lib/src/main/java/user/Categories.java
+++ lib/src/main/java/user/Categories.java
@@ -2,7 +2,20 @@ package user;
 
 import java.util.ArrayList;
 
-public class Categories {
-	ArrayList<String> comments = new ArrayList<String>();
-	// setter and getter methods 
+public class Categories extends Person {
+	
+	public Categories(String name, int age, String gender) {
+		super(name, age, gender);
+	}
+	
+	ArrayList<String> categories = new ArrayList<String>();
+
+	public ArrayList<String> getCategories() {
+		return categories;
+	}
+
+	public void setCategories(ArrayList<String> categories) {
+		this.categories = categories;
+	}
+	
 }
