3b416d3dd50293d4a3098126a61384f8146f79dc
hashi11002 <harshilkumar12@gmail.com>
2023-03-06 20:23:44 -0500

    Creating Junit for SQL

---
+++ lib/src/test/java/sql/SQLTester.java
@@ -0,0 +1,38 @@
+package sql;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+
+class SQLTester {
+
+	Connection con;
+	String url;
+	String user;
+	String password;
+	java.sql.Statement stmt;
+	ResultSet rs;
+	
+	@BeforeEach
+	void setup() throws SQLException{
+		url = "jdbc:mysql://localhost:3306/User_Data" ;
+		user = "root" ;
+		password = "" ;
+		java.sql.Statement stmt;
+		ResultSet rs;
+		con = DriverManager . getConnection ( url , user , password ) ;
+	}
+	
+	
+	
+	
+	
+}
