e1df403d6e3b125f084eab14948b739afa84143c
hashi11002 <harshilkumar12@gmail.com>
2023-03-06 20:26:09 -0500

    Rows tests

--- lib/src/test/java/sql/SQLTester.java
+++ lib/src/test/java/sql/SQLTester.java
@@ -52,6 +52,35 @@ class SQLTester {
 		assertEquals(3, rs.getInt(1)/2);
 	}
 	
+	@Test
+	void test_numberOfRows1() throws SQLException{
+		rs = con.createStatement().executeQuery("select name from person_info;");
+		List<String> names = new ArrayList<String>();
+
+        while (rs.next()) {
+            names.add(rs.getString(1));
+        }
+        
+        ResultSet rs2 = con.createStatement().executeQuery("select count(*) from person_info;");
+        rs2.next();
+        assertEquals(rs2.getInt(1), names.size());
+	}
+	
+	@Test
+	void test_numberOfRows2() throws SQLException{
+		rs = con.createStatement().executeQuery("select username from Login_Info;");
+		List<String> names = new ArrayList<String>();
+
+        while (rs.next()) {
+            names.add(rs.getString(1));
+        }
+        
+        ResultSet rs2 = con.createStatement().executeQuery("select count(*) from Login_Info;");
+        rs2.next();
+        assertEquals(rs2.getInt(1), names.size());
+	}
+	
+	
 	
 	
 }
