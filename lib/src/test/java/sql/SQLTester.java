d01623016604c53eb62ec6fbaa98f7604172998b
hashi11002 <harshilkumar12@gmail.com>
2023-03-06 20:25:09 -0500

    BeforeEach and AfterEach added

--- lib/src/test/java/sql/SQLTester.java
+++ lib/src/test/java/sql/SQLTester.java
@@ -8,6 +8,8 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -31,6 +33,10 @@ class SQLTester {
 		con = DriverManager . getConnection ( url , user , password ) ;
 	}
 	
+	@AfterEach
+	void tearDown() throws SQLException {
+		con.close();
+	}
 	
 	
 	
