2d5b9aaa89fc54327bc1cdb0b4499b35d39692de
eshavij595 <60937961+eshavij595@users.noreply.github.com>
2023-03-08 17:58:53 -0500

    Update LoginFrame.java
    Updated titles and sizes

--- lib/src/main/java/packageGUI/LoginFrame.java
+++ lib/src/main/java/packageGUI/LoginFrame.java
@@ -1,4 +1,4 @@
-package loginPageGUI;
+package packageGUI;
 
 import javax.swing.*;
 import java.awt.*;
@@ -8,14 +8,13 @@ import java.awt.event.ActionListener;
 public class LoginFrame extends JFrame implements ActionListener {
 
 	Container container = getContentPane();
-	JLabel titleLabel = new JLabel("The Daily News Reporter");
-	JLabel userLabel = new JLabel("Username:");
-	JLabel passwordLabel = new JLabel("Password:");
+	JLabel titleLabel = new JLabel("Welcome to The Daily News Reporter! "
+			+ "Here you can find the latest news! To get started, please login into your account!");
+	JLabel userLabel = new JLabel("Enter Username:");
+	JLabel passwordLabel = new JLabel("Enter Password:");
 	JTextField userTextField = new JTextField();
 	JPasswordField passwordField = new JPasswordField();
 	JButton loginButton = new JButton("LOGIN");
 	JButton resetButton = new JButton("RESET");
 	JCheckBox showPassword = new JCheckBox("Show Password");
 
-
-	
