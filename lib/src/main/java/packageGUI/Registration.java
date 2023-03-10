c790b38613fc4c1235f5067242b02e646ef36098
eshavij595 <60937961+eshavij595@users.noreply.github.com>
2023-03-09 21:34:24 -0500

    Uploaded Registration.java

---
+++ lib/src/main/java/packageGUI/Registration.java
@@ -0,0 +1,9 @@
+package registerPageGUI;
+
+public class Registration {
+    public static void main(String[] args)
+    {
+        //creating object of RegistrationForm class
+        new RegistrationForm();
+    }
+}
