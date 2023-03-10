77aa60ebde2bea1a04a051193512ddce9cc5d761
Nishiket Singh <69918255+Nishiket-singh@users.noreply.github.com>
2023-03-09 23:57:35 -0500

    Delete lib directory

--- lib/src/main/java/packageGUI/Registration.java
+++
@@ -1,9 +0,0 @@
-package packageGUI;
-
-public class Registration {
-    public static void main(String[] args)
-    {
-        //creating object of RegistrationForm class
-        new RegistrationForm();
-    }
-}
