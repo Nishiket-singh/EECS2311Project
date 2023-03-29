5ef2258e490e2507a7d555ded190ad1551b19cd6
eshavij595 <60937961+eshavij595@users.noreply.github.com>
2023-03-29 13:21:23 -0400

    Updated Main

--- 2311Project/src/GUI/Main.java
+++ 2311Project/src/GUI/Main.java
@@ -1,19 +1,14 @@
 package GUI;
 
 //********************************************
-
 import java.awt.event.*;
-
 import javax.swing.*;
-
 /// ********************************************
-
 public class Main extends JFrame {
 
 public static void main(String[] args) {
 
-//LaunchPage launchPage = new LaunchPage();
+LaunchPage launchPage = new LaunchPage();
 
 }
-
 }
