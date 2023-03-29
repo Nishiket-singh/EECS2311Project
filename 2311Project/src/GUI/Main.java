b755ea074c04a805d6d30a639cc3a13ca05fae5a
hashi11002 <harshilkumar12@gmail.com>
2023-03-29 13:15:43 -0400

    main

---
+++ 2311Project/src/GUI/Main.java
@@ -0,0 +1,19 @@
+package GUI;
+
+//********************************************
+
+import java.awt.event.*;
+
+import javax.swing.*;
+
+/// ********************************************
+
+public class Main extends JFrame {
+
+public static void main(String[] args) {
+
+//LaunchPage launchPage = new LaunchPage();
+
+}
+
+}
