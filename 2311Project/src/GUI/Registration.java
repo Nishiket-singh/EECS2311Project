f5915c97173fd003c822e5d4541a9dc298ce3532
eshavij595 <60937961+eshavij595@users.noreply.github.com>
2023-03-29 13:09:42 -0400

    Delete Registration.java
    Merging to Combine

--- 2311Project/src/GUI/Registration.java
+++
@@ -1,9 +0,0 @@
-package GUI;
-
-public class Registration {
-    public static void main(String[] args)
-    {
-        //creating object of RegistrationForm class
-        new RegistrationForm();
-    }
-}
