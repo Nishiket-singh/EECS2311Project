ec1c896c93029afd32dfbe7438f934f36ba587eb
Nishiket Singh <69918255+Nishiket-singh@users.noreply.github.com>
2023-03-31 20:42:05 -0400

    adding general and health news

---
+++ 2311Project/src/Data/General.java
@@ -0,0 +1,36 @@
+package Data;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class General {
+	
+	public static final List<String> headlines = Arrays.asList("Which general election voters would now find Trump more palatable? ","Casey’s General Stores (CASY) Posted Higher Than Expected Results","Blue Buffalo Is Bouncing Back at General Mills. What Does It Mean for the Stock?");
+	
+	public static final List<String> articles = Arrays.asList("Former president Donald Trump’s response to news that he’d been indicted by a Manhattan grand jury was not lacking for enthusiasm.\r\n"
+			+ "\r\n"
+			+ "“I believe this Witch-Hunt will backfire massively on Joe Biden,” he said in a statement released by his 2024 presidential campaign. “The American people realize exactly what the Radical Left Democrats are doing here. Everyone can see it. So our Movement, and our Party — united and strong — will first defeat [Manhattan District Attorney] Alvin Bragg, and then we will defeat Joe Biden, and we are going to throw every last one of these Crooked Democrats out of office so we can MAKE AMERICA GREAT AGAIN!” There’s definitely an air of what-else-are-you-going-to-say here. He’s running for the presidency and faces the unprecedented challenge of being the first former chief executive of the United States to undergo a potential criminal trial. He’s putting a positive spin on it, as only Trump can.\r\n"
+			+ "\r\n"
+			+ "But he’s not the only one saying this — that the indictment will be a political benefit to him. And that’s probably true … at least until June 2024.\r\n"
+			+ "\r\n"
+			+ "Sign up for How To Read This Chart, a weekly data newsletter from Philip Bump\r\n"
+			+ "It’s well established that the divide in the Republican Party — enthusiasm for Trump’s approach vs. deep fatigue with it — hasn’t proved much of a barrier to his political success. His ascent in 2016 faced enormous, though belated, pushback from the party establishment, but, thanks in part to how the party’s primaries are stacked, he overcame it. Then, while running against an unpopular Democratic opponent, he squeaked into the White House.\r\n"
+			+ "\r\n"
+			+ "This is the key point, in fact. At no time has Trump ever been the pick of a majority of Americans, voters or not. He didn’t even win a majority of votes in the 2016 Republican primary. In 2018 and 2022, his presence (as manifested in the White House and in boisterous endorsements, respectively) weighed down his party’s electoral chances. He has figured out how to rev up his base of support to shape whom the Republican Party puts forward for November, but he has not proved particularly successful at getting those candidates elected to office.\r\n"
+			+ "\r\n"
+			+ "Let’s operate under the assumption that this time is different, that the unique nature and circumstances of the charges in New York might result in new support for Trump. Where might it come from?","an equity investment management company, released its “U.S. Mid Cap Growth Strategy” fourth-quarter investor letter. A copy of the same can be downloaded here. In the fourth quarter, the strategy returned 4.18% (net), compared to 6.90% for the Russell Midcap Growth Index. For the calendar year, the strategy returned -22.13% (net) compared to the benchmark’s return of -26.72%. Weakness in Consumer Staples, Energy, Health Care, Information Technology, and Real Estate holdings led to the underperformance of the portfolio in the quarter. In addition, please check the fund’s top five holdings to know its best picks in 2022.\r\n"
+					+ "\r\n"
+					+ "TimesSquare U.S. Mid Cap Growth Strategy highlighted stocks like Casey’s General Stores, Inc. (NASDAQ:CASY) in the Q4 2022 investor letter. Headquartered in Ankeny, Iowa, Casey’s General Stores, Inc. (NASDAQ:CASY) is a convenience store. On March 30, 2023, Casey’s General Stores, Inc. (NASDAQ:CASY) stock closed at $214.43 per share. One-month return of Casey’s General Stores, Inc. (NASDAQ:CASY) was 0.59%, and its shares gained 8.18% of their value over the last 52 weeks. Casey’s General Stores, Inc. (NASDAQ:CASY) has a market capitalization of $7.99 billion.\r\n"
+					+ "\r\n"
+					+ "TimesSquare U.S. Mid Cap Growth Strategy made the following comment about Casey’s General Stores, Inc. (NASDAQ:CASY) in its Q4 2022 investor letter:\r\n"
+					+ "\r\n"
+					+ "\"More positive was Casey’s General Stores, Inc. (NASDAQ:CASY), which operates over 2,000 gas stations and convenience stores across the Midwestern U.S. Results were higher than expected in the latest quarter and driven by better same store sales. We trimmed the position on its 11% share price appreciation.\"\r\n"
+					+ "\r\n"
+					+ "Casey’s General Stores, Inc. (NASDAQ:CASY) is not on our list of 30 Most Popular Stocks Among Hedge Funds. As per our database, 29 hedge fund portfolios held Casey’s General Stores, Inc. (NASDAQ:CASY) at the end of the fourth quarter which was 24 in the previous quarter.\r\n"
+					+ "\r\n"
+					+ "We discussed Casey’s General Stores, Inc. (NASDAQ:CASY) in another article and shared the list of high growth retail stocks that are profitable. In addition, please check out our hedge fund investor letters Q4 2022 page for more investor letters from hedge funds and other leading investors.","General Mills (NYSE: GIS) is a large and iconic consumer staples company. But it isn't exactly correct to view it as a food maker, because what it really owns is a collection of brands that it manages. Brand-level results vary, but the end goal is to keep growing each brand's business over time.\r\n"
+							+ "\r\n"
+							+ "General Mills' pet food division has been experiencing some pain of late, but this important division seems to be on the mend. Here's what's going on and why it is so important for General Mills. General Mills bought Blue Buffalo in 2018. At the time, some industry watchers were concerned that the consumer staples giant overpaid for the company, which was the leading provider of healthy pet food. Management, however, believed that it could speed up Blue Buffalo's effort to move from the specialty store space into mass market outlets.Buying Blue Buffalo was all about the growth potential it offered to General Mills. Overall, the results have been exceptional, with General Mills expanding sales at Blue Buffalo at a compound annual rate of 15% between fiscal 2018 and fiscal 2022. This is exactly what adding a smaller brand with limited distribution can achieve when a larger company with more advertising and distribution clout buys it.\r\n"
+							+ "\r\n"
+							+ "But even good businesses don't grow to the sky, with performance over time more likely to resemble a sine curve than a straight line. The best that can really be hoped for is an upward sloping sine curve. And that means that, sometimes, things will go wrong. That's exactly what the last couple of quarters have shown for Blue Buffalo.");
+}
---
+++ 2311Project/src/Data/Health.java
@@ -0,0 +1,62 @@
+package Data;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Health {
+	
+	public static final List<String> headlines = Arrays.asList("Montreal public health calls for vigilance as 2 new cases of mpox detected","Health department board to consider proposal to limit pandemic responses","Experts Weigh In: Is Soy Bean Oil Good for Your Health?");
+	public static final List<String> articles = Arrays.asList("Montreal public health detected two cases of mpox and is calling on health-care professionals and the general population to remain vigilant.\r\n"
+			+ "\r\n"
+			+ "Although the World Health Organization (WHO) still considers the global situation related to mpox, formerly known as monkeypox, to be a public health emergency of international concern, Quebec's Ministry of Health had announced on Feb. 14 that the outbreak of mpox had been contained.Montreal public health says in a news release that the two new patients are men who had sex with men, likely outside the country. One had one dose of the vaccine, while the other had two, the release says.\r\n"
+			+ "\r\n"
+			+ "The Imvamune vaccine reduces the risk of infection and the severity of the disease. The vaccination rate of people infected with the virus is 54 percent for the first dose and 24 percent for the second, the release says.\r\n"
+			+ "\r\n"
+			+ "Even though public health officials believe the risks of local transmission are low following these two cases, they recommend that at-risk individuals get vaccinated.\r\n"
+			+ "\r\n"
+			+ "\"Our vaccination rates could really be improved. It would allow us to be better protected and prevent the risk of local transmission,\" explained Dr. Geneviève Bergeron, chief medical officer of Montreal public health's prevention and control of infectious diseases department.\r\n"
+			+ "\r\n"
+			+ "\"There is no vaccine that is 100 percent effective, but we know the vaccine is effective. Among people who have received the vaccine, we see presentations that are less severe. Fewer lesions, fewer complications.\"\r\n"
+			+ "\r\n"
+			+ "The virus is transmitted through direct contact of the skin or mucous membranes with the lesions of an infected person or their bodily fluids. It can also be transmitted through respiratory droplets during prolonged close contact.\r\n"
+			+ "\r\n"
+			+ "The main symptoms are initially fever, headaches, fatigue, chills, body aches, and lymph node inflammation. \r\n"
+			+ "\r\n"
+			+ "Subsequently, there is the appearance of skin eruptions on the face and elsewhere on the body. These skin eruptions can be extremely painful.\r\n"
+			+ "\r\n"
+			+ "Between May and October 2022, 400 probable and confirmed cases of mpox were diagnosed in Montreal. Throughout Quebec, there were 526 probable or confirmed cases.\r\n"
+			+ "\r\n"
+			+ "The last appearances of mpox in Montreal date back to the fall of 2022.\r\n"
+			+ "\r\n"
+			+ "Public health estimates that about 54 percent of the at-risk population (men who have sex with men) have had a first dose, but only 24 percent have had a second dose.\r\n"
+			+ "\r\n"
+			+ "As cases tick upwards in other cities, one Montreal men's health organization, RÉZO, is reviving an old awareness campaign. \r\n"
+			+ "\r\n"
+			+ "\"We are there to listen and to inform again about the transmission mode, about what mpox is and what are the symptoms and to continue to give the information about the vaccination sites,\" said director general Alexandre Dumont Blais.\r\n"
+			+ "\r\n"
+			+ "Montreal public health says those eligible for the vaccine can make an appointment through the Clic Santé website or show up at a walk-in clinic.","The board of health for the Health Department of Northwest Michigan is scheduled to consider a resolution that could affect the department's responses to health emergencies like COVID-19.\r\n"
+					+ "\r\n"
+					+ "At the April 4 meeting in Charlevoix, the eight board members who are county commissioners from the health department's jurisdiction of Antrim, Charlevoix, Emmet and Otsego counties, are scheduled to review a resolution upholding medical freedom in light of the recent COVID-19 countermeasure experience authored by Rich Ginop, chairman of the Emmet County Board of Commissioners.\r\n"
+					+ "\r\n"
+					+ "The resolution states that all state or local governmental emergency measures affecting, or emanating from the health department regarding a pandemic or epidemic, shall expire after 28 days and only a three-fourths vote of the full board of health can authorize continued implementation of such measures, and then only on a month-by-month basis. The resolution also states that the health department shall not impose, permit or enforce Covid vaccine mandates, the mandated masking or testing of asymptomatic persons.\r\n"
+					+ "\r\n"
+					+ "When asked if the 28-day expiration for health measures can be legally applied to mandates from the Michigan Department of Health and Human Services (MDHHS) or the federal government, Ginop said \"I am not sure.\"\r\n"
+					+ "\r\n"
+					+ "\"Courts have consistently upheld MDHHS’s ongoing authority under the public health code. MDHHS will continue to use its powers under the Public Health Code to protect the lives of the people of Michigan,\" said Chelsea Wuth, a spokeswoman with MDHHS when asked to respond to the resolution.\r\n"
+					+ "\r\n"
+					+ "Matthew Cross of the Plunkett Cooney law firm, who serves as legal advisor for the board of health, declined to comment.\r\n"
+					+ "\r\n"
+					+ "\"I have not had an opportunity to look over the resolution since receiving it last week and likely won’t have an opportunity to do so until closer to the meeting,\" Cross said in an email on March 27.\r\n"
+					+ "\r\n"
+					+ "Last December, the state court of appeals ruled that the Ottawa County Health Officer acted legally and appropriately when she issued a pre-K-6 mask mandate during the height of the COVID-19 pandemic.\r\n"
+					+ "\r\n"
+					+ "\"I am sick and tired of the so-called authorities lying to us and not revealing the truth about vaccine damages, deaths and injuries that are happening,\" Ginop responded when asked what motivated him to draft the resolution.\r\n"
+					+ "\r\n"
+					+ "According to a 2022 report from Johns Hopkins Medicine titled \"Is the COVID-19 vaccine safe?,\" authors Lisa Maragakis and Gabor David Kelen, both MDs, said the Pfizer and Moderna vaccines authorized by the U.S. Food and Drug Administration and recommended by the Centers for Disease Control and Prevention, \"are very safe and very good at preventing serious or fatal cases of COVID-19. The risk of serious side effects associated with these vaccines is very small.\"Ginop believes he has the votes on the board to gain approval of the resolution. He bristled when asked if he was over reaching.\r\n"
+					+ "\r\n"
+					+ "\"They over reached (and) they are shoving it down our throats. They are spending tax dollars in the mainstream media that (the vaccine) is good for you and it's effective. There is more evidence against it then there is for it,\" he said.\r\n"
+					+ "\r\n"
+					+ "In addition to Ginop, other board members include Jarris Rubingh and Dawn LaVanway of Antrim County, Josh Chamberlain and Scott Hankins of Charlevoix County, Don Mapes from Emmet County and Henry Mason and Jonathan Turnbull of Otsego County.","The health benefits of soybeans oil are such that it helps to develop good body immune system, strengthens body tissues and organs, maintains brain function, maintains eye health, keeps teeth strong and healthy it keeps the digestive system healthy, maintains steady blood pressure, prevents osteoporosis, controlled transition of menopause, lowers the risk of breast and colon cancer, prevents anaemia");
+
+}
+
